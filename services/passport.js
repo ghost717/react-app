4f97beeffb9e0a084493141d89f88f27dce0b203
ghost717 <dawid.zawadzki7717@gmail.com>
2018-02-10 21:31:56 +0100

    npm install react react-redux- react-router-dom

--- services/passport.js
+++ services/passport.js
@@ -28,12 +28,10 @@ passport.use(
 		
 		if(existingUser){
 			done(null, existingUser);
-		} else {
-			const user = await new User({ googleId : profile.id, name: profile.displayName }).save();
-			done(null, user);
 		}
 		
-
+		const user = await new User({ googleId : profile.id, name: profile.displayName }).save();
+		done(null, user);
 
 		console.log('accessToken: ', accessToken);
 		console.log('refreshToken: ', refreshToken);
